0eb67054e4c8b4ffebb976e8aca87f6f26f7aafe
Lars Lorenz <lars.ll.lorenz@gmail.com>
2022-07-30 19:17:30 +0200

    Show gin tonic table

--- gintonicmatrix/matrix/views.py
+++ gintonicmatrix/matrix/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
-from .models import GinTonicEvaluation
+from .models import Gin, GinTonicEvaluation, Tonic
 
 
 def index(request):
-    evaluations = GinTonicEvaluation.objects.all()
+    gins = Gin.objects.all()
+    tonics = Tonic.objects.all()
+    evaluation_rows = []
+    for tonic in tonics:
+        evaluation_row = []
+        for gin in gins:
+            evaluations = GinTonicEvaluation.objects.filter(gin=gin, tonic=tonic)
+            mean = 0.0
+            for evaluation in evaluations:
+                mean += evaluation.rating / len(evaluations)
+            evaluation_row.append(mean)
+        evaluation_rows.append((tonic, evaluation_row))
     return render(request, "matrix/list.html", locals())

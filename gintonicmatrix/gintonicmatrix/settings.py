1640683ace90a8d9b69af6d073cc864c96951175
Lars Lorenz <lars.ll.lorenz@gmail.com>
2022-07-31 17:37:41 +0200

    Change ALLOWED_HOSTS to an env variable

--- gintonicmatrix/gintonicmatrix/settings.py
+++ gintonicmatrix/gintonicmatrix/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://gin-tonic-matrix.herokuapp.com/"]
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
 
 
 # Application definition

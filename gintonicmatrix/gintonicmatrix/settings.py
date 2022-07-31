78bfa89118f738a8b08c3b36d29fb7f17afcb365
Lars Lorenz <lars.ll.lorenz@gmail.com>
2022-07-31 17:12:06 +0200

    Add STATIC_ROOT

--- gintonicmatrix/gintonicmatrix/settings.py
+++ gintonicmatrix/gintonicmatrix/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'matrix/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
